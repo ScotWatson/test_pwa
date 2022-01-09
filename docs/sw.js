1c41536c1aaa2dbe1e5e3f8669b07b92cfe65e64
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-09 17:01:14 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,13 +1,17 @@
 // (c) 2021 Scot Watson  All Rights Reserved
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+function projectFile(filename) {
+  return window.location.pathname + filename;
+}
+
 function selfInstall(e) {
   function addCaches(cache) {
     cache.addAll([
-      "/",
-      "/index.html",
-      "/index.js",
-      "/style.css",
+      projectFile(""),
+      projectFile("index.html"),
+      projectFile("index.js"),
+      projectFile("style.css"),
     ])
   }
   e.waitUntil(caches.open("store").then(addCaches));
