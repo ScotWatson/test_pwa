0e8c8b45da1ca2901ee2dfc5219ff2f5e3891ee1
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-09 17:52:56 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-// (c) 2021 Scot Watson  All Rights Reserved
+// (c) 2022 Scot Watson  All Rights Reserved
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 function projectFile(filename) {
@@ -6,9 +6,13 @@ function projectFile(filename) {
 }
 
 function notify() {
-  alert("I am here.");
+  if (document.body.style.backgroundColor === "#404040") {
+    document.body.style.backgroundColor === "#C0C0C0";
+  } else {
+    document.body.style.backgroundColor === "#404040";
+  }
 }
-setInterval(notify, 10000);
+setInterval(notify, 2000);
 
 // Register service worker to control making site work offline
 
