2fcec899053be6ad1f8f16cbaddf1d5ce8519672
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-09 17:29:13 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,8 +1,11 @@
 // (c) 2021 Scot Watson  All Rights Reserved
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+// NOTE: This path is hardcoded, as the service worker cannot access window.location
+const pathname = "/hello-world/";
+
 function projectFile(filename) {
-  return window.location.pathname + filename;
+  return pathname + filename;
 }
 
 function selfInstall(e) {
