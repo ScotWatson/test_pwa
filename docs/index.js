a8d49d8397bf9523797e04b5803814fc4ebc14ed
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-09 19:23:54 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,15 +1,17 @@
 // (c) 2022 Scot Watson  All Rights Reserved
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+alert("Start");
+
 function projectFile(filename) {
   return window.location.pathname + filename;
 }
 
 function notify() {
   if (document.body.style.backgroundColor === "#404040") {
-    document.body.style.backgroundColor === "#C0C0C0";
+    document.body.style.backgroundColor = "#C0C0C0";
   } else {
-    document.body.style.backgroundColor === "#404040";
+    document.body.style.backgroundColor = "#404040";
   }
 }
 setInterval(notify, 2000);
