decc17738c339fc3d0b14e8c36a4e6cc365211f0
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-11 19:44:37 -0500

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-// (c) 2021 Scot Watson  All Rights Reserved
+// (c) 2022 Scot Watson  All Rights Reserved
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // NOTE: This path is hardcoded, as the service worker cannot access window.location
@@ -9,22 +9,28 @@ function projectFile(filename) {
 }
 
 function selfInstall(e) {
+  console.log("sw.js: Start Installing");
   function addCaches(cache) {
+    console.log("sw.js: Start Adding Caches");
     cache.addAll([
       projectFile(""),
       projectFile("index.html"),
       projectFile("index.js"),
       projectFile("style.css"),
     ])
+    console.log("sw.js: End Adding Caches");
   }
   e.waitUntil(caches.open("store").then(addCaches));
+  console.log("sw.js: End Installing");
 }
 
 function selfFetch(e) {
+  console.log("sw.js: Start Handling Fethch");
   function sendResponse(response) {
     return response || fetch(e.request);
   }
   e.respondWith(caches.match(e.request).then(sendResponse));
+  console.log("sw.js: End Handling Fethch");
 }
 
 self.addEventListener("install", selfInstall);
