f7c9b93fe3b307343bddd55b7136927893f3e5f9
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-01-12 23:05:26 -0500

    Create makeManifest.js

---
+++ docs/makeManifest.js
@@ -0,0 +1,20 @@
+let webManifest = {};
+webManifest.description = "Bare minimum to provide a PWA  (c) 2022 Scot Watson  All Rights Reserved";
+webManifest.display = "fullscreen";
+webManifest.icons = [];
+webManifest.icons.push({
+  "src": "icon.png",
+  "sizes": "192x192",
+  "type": "image/png"
+});
+webManifest.name = "Test Web App";
+webManifest.short_name = "PWA";
+webManifest.start_url = "/test_pwa/index.html";
+let strWebManifest = JSON.stringify(webManifest);
+console.log(strWebManifest);
+let blobWebManifest = new Blob( [ strWebManifest ] );
+let linkWebManifest = document.createElement("link");
+let urlWebManifest = URL.createObjectURL(blobWebManifest);
+linkWebManifest.setAttribute("rel", "manifest");
+linkWebManifest.setAttribute("href", urlWebManifest);
+document.head.appendChild(linkWebManifest);
